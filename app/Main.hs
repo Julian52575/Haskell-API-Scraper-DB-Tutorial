a702406323812fdd31c99acb0c2b86bf0af1a1b8
julian <julian.bottiglione@epitech.eu>
2025-04-19 00:28:03 +0200

    [wip] Reading Env variable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,26 @@
+{-- Copyright julian.bottiglione@epitech.eu
+-- Main
+--}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE FlexibleContexts #-}  -- For MonadReader Env
+--{-# LANGUAGE ScopedTypeVariables #-}  -- Typed variables
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.Text (Text, unpack)
+import Data.Functor (void)
+import Control.Monad.Reader.Class (MonadReader, asks)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Reader (runReaderT, ReaderT)
+import Control.Monad.Trans (lift)
+
+import Env (Env, startingMessage, initEnvFromDotEnv)
+
+printMsgFromEnv :: (MonadReader Env m, MonadIO m) => m ()
+printMsgFromEnv = do
+  msgRaw <- asks startingMessage
+  liftIO $ putStrLn (unpack msgRaw)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  env <- initEnvFromDotEnv
+  runReaderT printMsgFromEnv env
