089f5a275ce176463be3ba8054d319c5fdc4582b
julian <julian.bottiglione@epitech.eu>
2025-04-21 15:23:26 +0200

    [fix] Running app under runReaderT and removed redundant imports

--- app/Main.hs
+++ app/Main.hs
@@ -6,24 +6,15 @@
 --{-# LANGUAGE ScopedTypeVariables #-}  -- Typed variables
 module Main where
 
-import Data.Text (unpack)
-import Control.Monad.Reader.Class (MonadReader, asks)
-import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (runReaderT)
 import Network.Wai.Handler.Warp (run)
 
-import Env (Env, startingMessage, initEnv)
+import Env (initEnv)
 import Api.Server (appM)
-
-
-printMsgFromEnv :: (MonadReader Env m, MonadIO m) => m ()
-printMsgFromEnv = do
-  msgRaw <- asks startingMessage
-  liftIO $ putStrLn (unpack msgRaw)
 
 main :: IO ()
 main = do
   env <- initEnv
   app <- appM
-  runReaderT printMsgFromEnv env
-  run 8080 app
+  runReaderT (liftIO $ run 8080 app) env
